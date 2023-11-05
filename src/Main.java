e9f4615c58de2b515d56a65e0f931af00db9a008
Mateusz <mateusz.krzykawski@edu.uekat.pl>
2023-11-05 20:15:34 +0100

    Czysta szachownica 1.0

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,52 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int X =8;
-        int Y =8;
+        int X = 8;
+        int Y = 8;
 
         int[][] Szachownica = new int[X][Y];
         Szachownica(Szachownica);
-        }
-        public static void Szachownica(int[][] Szachownica) {
+    }
 
+    public static void Szachownica(int[][] Szachownica) {
 
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 16; j++) {
-                    if (j%2==0)
+        char znak = 72;
+        System.out.println();
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.print(' ');
+
+                for (int j = 1; j < 18; j++)
+                {
+                    if ((j==1 && i!=0 && i!=9))
                     {
-                        char znak = 65;
+                        System.out.print(i);
+                        System.out.print('|');
+                    }
+                    else if (j==1)
+                    {
+                        System.out.print(" |");
+                    }
+                    else if ((j % 2 == 0 && i == 0) || (j % 2 == 0 &&  i==9))
+                    {
                         System.out.print(znak);
+                        znak--;
                     }
-                    else
+                    else if (j%2==0)
+                        {
+                            System.out.print(' ');
+                        }
+                        else
                     {
-                        System.out.print('+');
+                        System.out.print('|');
                     }
-
+                    if ((j==17 && i!=0 && i!=9))
+                    {
+                        System.out.print(i);
+                    }
                 }
-                System.out.println('-');
-            }
+                znak = 72;
+                System.out.println();
         }
     }
+}
