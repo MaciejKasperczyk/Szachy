18b229373748a8e6e2a9786eeb0b0569219a1169
Maciej Kasperczyk <makasperczyk1@gmail.com>
2023-11-07 17:58:06 +0100

    added Pionek

--- src/Figury.java
+++ src/Figury.java
@@ -1,3 +1,22 @@
 abstract class Figury {
 
 }
+class Pionek
+{
+
+    // Koordynaty by wiedziec gdzie Pionek ma powstać
+    int wspolrzedneX;
+    int getWspolrzedneY;
+
+
+    public boolean czyZbyty = False;
+    public enum kolor
+    {
+        bialy,
+        czarny
+    }
+
+
+
+
+}
