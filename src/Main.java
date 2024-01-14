b84a26ba1f19d9b39cb5c6334a367efb72a50cef
Mateusz <mateusz.krzykawski@edu.uekat.pl>
2024-01-14 12:26:00 +0100

    Menu gry

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,26 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 
 
+import java.util.Scanner;
+
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Scanner tekst = new Scanner(System.in);
         Gra gra1 = new Gra();
-        gra1.rozgrywka();
+        System.out.println("Witaj w grze w szachy");
+        do {
+            System.out.println("Wybierz:\n1 - zapoznanie się z zasadami gry\n2 - rozpocznij partię\n3 - zakończ program");
+            byte x = tekst.nextByte();
+            if (x==1){
+                System.out.println("Zasady gry .................");
+                System.out.println(1);
+            }
+            else if (x==2) {
+                gra1.rozgrywka();
+            }
+            else break;
+        } while (true);
+        System.out.println("Do zobaczenia!!!");
     }
     }
